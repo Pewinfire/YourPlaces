9718ca5e5c8d747138cf1f7a5ccef93d9091fe58
Pewinfire <s.dung@hotmail.es>
2022-01-18 08:03:47 +0100

    .

--- Yourplaces_cl/src/App.js
+++ Yourplaces_cl/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -6,14 +6,22 @@ import {
   Switch,
 } from "react-router-dom";
 
-import Users from "./user/pages/User";
+/* import Users from "./user/pages/User";
 import NewPlace from "./places/pages/NewPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
+
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
-import Authenticate from "./user/pages/Authenticate";
+import Authenticate from "./user/pages/Authenticate"; */
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
+
+const Users = React.lazy(() => import("./user/pages/User"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlaces"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Authenticate = React.lazy(() => import("./user/pages/Authenticate"));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -66,7 +74,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
